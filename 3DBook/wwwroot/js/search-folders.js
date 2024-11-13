71a1a8dc1821e1680faf46552d990037999c236f
Danijel Boksan <danijel.boksan@live.com>
2024-11-13 14:37:47 +0100

    Manage to filter data with js on Index page of Folders

---
+++ 3DBook/wwwroot/js/search-folders.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("#folds, #machine, #enter, #exit").keyup(function () {
+        var getFilter = function (el, selector) {
+            var txt = $(el).val().toLowerCase();
+            return txt
+                ? function (i, p) { return $(p).find(selector).text().toLowerCase().indexOf(txt) !== -1; }
+                : function (i, p) { return true; };
+        };
+
+        $('.toHide').hide()
+           // .filter(getFilter('#srcInternalCode', '.panel-heading'))
+            .filter(getFilter('#machine', '#destMachine'))
+            .filter(getFilter('#folds', '#destFolds'))
+            .filter(getFilter('#enter', '#destEnter'))
+            .filter(getFilter('#exit', '#destExit'))
+            .show();
+    });
+});
