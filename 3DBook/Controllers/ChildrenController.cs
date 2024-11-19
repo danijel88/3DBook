bcc379f0e76f23b8296eb6273e466194de9e68ab
Danijel Boksan <danijel.boksan@live.com>
2024-11-19 09:14:36 +0100

    Adding validation to the controller

--- 3DBook/Controllers/ChildrenController.cs
+++ 3DBook/Controllers/ChildrenController.cs
@@ -2,6 +2,8 @@
 using System.Reflection;
 using _3DBook.Models.ChildrenViewModels;
 using _3DBook.UseCases.FolderAggregate;
+using _3DBook.UseCases.FolderAggregate.Validators;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -33,6 +35,14 @@ public class ChildrenController(IChildrenService childrenService, IWebHostEnviro
     [HttpPost("Children/{folderId}/Create")]
     public async Task<IActionResult> Create(int folderId, CreateChildrenViewModel model, IFormFile file)
     {
+        var validator = new CreateChildrenValidator();
+        var validationResult = await validator.ValidateAsync(model);
+        if (!validationResult.IsValid)
+        {
+            validationResult.AddToModelState(this.ModelState);
+            return View(model);
+        }
+
         if (file.Length == 0)
         {
             ModelState.AddModelError("Error", "File must be selected.");
---
+++ 3DBook/UseCases/FolderAggregate/Validators/CreateChildrenValidator.cs
@@ -0,0 +1,21 @@
+﻿using _3DBook.Models.ChildrenViewModels;
+using FluentValidation;
+
+namespace _3DBook.UseCases.FolderAggregate.Validators;
+
+public class CreateChildrenValidator : AbstractValidator<CreateChildrenViewModel>
+{
+    public CreateChildrenValidator()
+    {
+        RuleFor(x => x.ElasticSize)
+            .GreaterThan(-1);
+        RuleFor(x => x.MouthLength)
+            .GreaterThan(-1);
+        RuleFor(x => x.MouthWidth)
+            .GreaterThan(-1);
+        RuleFor(x => x.Thickness)
+            .GreaterThan(-1);
+        RuleFor(x => x.FolderId)
+            .GreaterThan(0);
+    }
+}
